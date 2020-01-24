907e6c5694390e8991de7c786b26e67580190b79
Uchoosecode <awittyl44@gmail.com>
2020-01-24 16:28:51 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Mr. Chocolate", 5, "male", "brown", "kind", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Rowdy", 4, "female", "yellow", "warm", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Tabitha", 2, "female", "pink", "cheerful", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Sergeant Brown", 4, "male", "green", "lucky", "false");
+INSERT INTO (name, age, gender, color, temperament, alive) 
+VALUES ("Melissa", 6, "female", "blue", "grumpy", "true");
+
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) 
+-- VALUES ("Grumpy" 8, "male", "blue", "grumpy", "yes");
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) 
+-- VALUES ("Wish", 3, "female", "blue-green", "hopeful", "yes");
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) 
+-- VALUES ("Destruction", 10, "male", "red", "evil", "no");
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) 
+-- VALUES ("Bedtime", 5, "male",  "blue", "sleepy", "yes");
