66059834865967292cc60ad2d5c83542b0efd669
Uchoosecode <awittyl44@gmail.com>
2020-01-24 20:50:24 -0500

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,20 +1,27 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ("Mr. Chocolate", 5, "male", "brown", "kind", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ("Rowdy", 4, "female", "yellow", "warm", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ("Tabitha", 2, "female", "pink", "cheerful", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ("Sergeant Brown", 4, "male", "green", "lucky", "false");
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ("Melissa", 6, "female", "blue", "grumpy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 15, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 10, 'M', 'dark brown', 'rowdy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 13, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'brown', 'grumpy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 99, 'F', 'brown', 'deadly', 0);
+
+
+
+
+
+
+
+
 
 
 -- INSERT INTO bears (name, age, gender, color, temperament, alive) 
--- VALUES ("Grumpy" 8, "male", "blue", "grumpy", "yes");
--- INSERT INTO bears (name, age, gender, color, temperament, alive) 
--- VALUES ("Wish", 3, "female", "blue-green", "hopeful", "yes");
--- INSERT INTO bears (name, age, gender, color, temperament, alive) 
--- VALUES ("Destruction", 10, "male", "red", "evil", "no");
--- INSERT INTO bears (name, age, gender, color, temperament, alive) 
--- VALUES ("Bedtime", 5, "male",  "blue", "sleepy", "yes");
+-- VALUES ('Mr. Chocolate', 5, 'male', 'brown', 'kind', 'true'),
+--      ('Rowdy', 4, 'male', 'yellow', 'warm', 'true'),
+--      ('Tabitha', 2, 'female', 'pink', 'cheerful', 'true'),
+--      ('Sergeant Brown', 4, 'male', 'green', 'lucky', 'false'),
+--      ('Melissa', 6, 'female', 'blue', 'grumpy', 'true'),
+--      ('Grinch', 4, 'male', 'green', 'lucky', 'false'), 
+--      ('Wendy', 6, 'female', 'blue', 'grumpy', 'true'),   
+--      (NULL, 4, 'male', 'green', 'lucky' 'false');
